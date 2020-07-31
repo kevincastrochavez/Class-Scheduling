6cb77efbc282fc7988b7767e71a47f9918a47db2
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-31 17:44:17 -0500

    setup project and cours library componet

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import CourseLibrary from './courseLibrary';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux</h2>
+        <h1>Course Library</h1>
+        <CourseLibrary/>
       </div>
     );
   }
